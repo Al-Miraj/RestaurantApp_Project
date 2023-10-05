fae8cf0b05bef90437d5e98f32fecf43994cb1a0
ManuvonFranquemon <145396958+ManuvonFranquemon@users.noreply.github.com>
2023-10-05 12:29:34 +0200

    Add files via upload

---
+++ RestaurantApp/MenuItem.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+
+class MenuItem
+{
+    public string Name { get; set; }
+    public string Details { get; set; }
+    public double Price { get; set; }
+    public string? Icon;
+
+    public MenuItem(string name, string details, double price)
+    {
+        Name = name;
+        Details = details;
+        Price = price;
+        GetAppropiateIcon();
+    }
+
+    public void GetAppropiateIcon()
+    {
+        string[] icons = Details.Split(" ");
+        if (icons[0] == "Beef")
+            Icon = "";
+    }
+}
